c3952f0cfdfeb48467b319c101dd45f6d0059139
Narayan <artawdwasa@gmail.com>
2026-07-11 22:29:07 +0700

    fix passport

--- lib/passport.ts
+++ lib/passport.ts
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: `${BACKEND_URL}/api/auth/google/callback`,
+      callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`,
     },
     async (_accessToken, _refreshToken, profile, done) => {
       try {
